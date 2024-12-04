2c0ee89aa73851e0b7c4b7b83d90e65777697502
Ivan Ostrovsky <ostrovskyiml@gmail.com>
2024-12-04 03:13:22 +0300

    created_a_Binary_Tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,44 +1,138 @@
 const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+// Узел 
+class Node {
+  constructor(data) {
+    this.data = data; // значение узла
+    this.left = null; 
+    this.right = null; 
+  }
+}
 
-/**
- * Implement simple binary search tree according to task description
- * using Node from extensions
- */
 module.exports = class BinarySearchTree {
+  constructor() {
+    this.rootNode = null; // начало - пустой корень
+  }
+
+  
   root() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  // новый узел
+  add(data) {
+    const newNode = new Node(data);
+
+    if (!this.rootNode) {
+      this.rootNode = newNode; // если дерево пустое, новый узел = корень
+      return;
+    }
+
+    let current = this.rootNode;
+
+    while (true) {
+      if (data < current.data) {
+        
+        if (!current.left) {
+          current.left = newNode; 
+          return;
+        }
+        current = current.left;
+      } else if (data > current.data) {
+        
+        if (!current.right) {
+          current.right = newNode; 
+          return;
+        }
+        current = current.right;
+      } else {
+        
+        return;
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  // Проверка наличия узла
+  has(data) {
+    let current = this.rootNode;
+
+    while (current) {
+      if (data === current.data) return true;
+      current = data < current.data ? current.left : current.right;
+    }
+
+    return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  // Поиск узла
+  find(data) {
+    let current = this.rootNode;
+
+    while (current) {
+      if (data === current.data) return current;
+      current = data < current.data ? current.left : current.right;
+    }
+
+    return null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  // Удаление узла
+  remove(data) {
+    this.rootNode = this._removeNode(this.rootNode, data);
   }
 
+  _removeNode(node, data) {
+    if (!node) return null;
+
+    if (data < node.data) {
+      node.left = this._removeNode(node.left, data);
+      return node;
+    } else if (data > node.data) {
+      node.right = this._removeNode(node.right, data);
+      return node;
+    } else {
+      // У узла нет потомков
+      if (!node.left && !node.right) {
+        return null;
+      }
+      // У узла один потомок
+      if (!node.left) return node.right;
+      if (!node.right) return node.left;
+
+      // У узла два потомка
+      let minRight = this._findMin(node.right);
+      node.data = minRight.data;
+      node.right = this._removeNode(node.right, minRight.data);
+      return node;
+    }
+  }
+
+  // мин узел
   min() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    if (!this.rootNode) return null;
+
+    let current = this.rootNode;
+    while (current.left) {
+      current = current.left;
+    }
+    return current.data;
   }
 
+  // макс узел
   max() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    if (!this.rootNode) return null;
+
+    let current = this.rootNode;
+    while (current.right) {
+      current = current.right;
+    }
+    return current.data;
+  }
+
+  _findMin(node) {
+    while (node.left) {
+      node = node.left;
+    }
+    return node;
   }
 };
